a8ed1cf5aa668ca2d9dcc3e339b0d969f1a26941
Pather <nathaniel.pather@riotinto.com>
2026-04-21 10:07:26 +1000

    Add hello component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import HelloComponent from "./HelloComponent";
+
 export default function App() {
+
+
   return (
-    <div>hello</div>
+    <div>
+      <HelloComponent name="billy" />
+      <HelloComponent name="Sandy"/>
+      <HelloComponent name="bob"/>
+    </div>
   )
 }
---
+++ src/HelloComponent.tsx
@@ -0,0 +1,6 @@
+  export default function HelloComponent(props: {name: string}) {
+    return (
+        <div>Hello {props.name}!</div>
+    )
+  }
+  
